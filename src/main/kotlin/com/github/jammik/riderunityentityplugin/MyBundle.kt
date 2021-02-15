50c43f68f2b3c3e803a68c75aa8c0ff60d8f58e8
GitHub Action <action@github.com>
2021-02-15 23:03:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/jammik/riderunityentityplugin/MyBundle.kt
+++ src/main/kotlin/com/github/jammik/riderunityentityplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jammik.riderunityentityplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/jammik/riderunityentityplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jammik.riderunityentityplugin.services
+
+import com.github.jammik.riderunityentityplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
